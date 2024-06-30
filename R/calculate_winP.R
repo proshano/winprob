c4df4925e74b85d5120d21d1621afd74772ae134
Pavel Roshanov <pavelroshanov@gmail.com>
2024-06-30 13:07:25 -0400

    added link

--- R/calculate_winP.R
+++ R/calculate_winP.R
@@ -1,6 +1,7 @@
 #' Calculate Win Probability and Other Metrics
 #' R package written by Pavel S Roshanov June 29 2024
 #' Based on idea and SAS code in Zou G, Zou L, Q S-F. Parametric and nonparametric methods for confidence intervals and sample size planning for win probability in parallel-group randomized trials with Likert item and Likert scale data. Pharmaceutical Statistics. 2023 May 1;22(3):418–39.
+#'https://onlinelibrary.wiley.com/doi/10.1002/pst.2280
 #'
 #' This function calculates the Win Probability (WinP) and its confidence interval
 #' along with other related metrics.
