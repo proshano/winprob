93f944beee3868ca428ffc2d15efcdba795fb0c8
Pavel Roshanov <pavelroshanov@gmail.com>
2024-06-30 12:19:47 -0400

    some updates

--- R/calculate_winP.R
+++ R/calculate_winP.R
@@ -1,6 +1,6 @@
 #' Calculate Win Probability and Other Metrics
 #' R package written by Pavel S Roshanov June 29 2024
-#' Based on idea and SAS code in Zou G, Zou L, Qiu S fang. Parametric and nonparametric methods for confidence intervals and sample size planning for win probability in parallel-group randomized trials with Likert item and Likert scale data. Pharmaceutical Statistics. 2023 May 1;22(3):418–39.
+#' Based on idea and SAS code in Zou G, Zou L, Qiu S Fang. Parametric and nonparametric methods for confidence intervals and sample size planning for win probability in parallel-group randomized trials with Likert item and Likert scale data. Pharmaceutical Statistics. 2023 May 1;22(3):418–39.
 #'
 #' This function calculates the Win Probability (WinP) and its confidence interval
 #' along with other related metrics.
@@ -19,16 +19,16 @@
 #'   pre = c(18, 27, 16, 17, 15, 20, 16, 28, 28, 25, 24, 16, 26, 21, 21, 22, 26, 19, 22, 16, 21, 20, 17, 22, 19, 21, 18, 21, 27, 15, 24, 15, 17, 20, 18, 28, 21, 18, 27.46, 19, 20, 16, 21, 23, 23, 24, 25, 22, 20, 20, 25, 18, 26, 20, 17, 22, 22, 23, 17, 22, 26),
 #'   post = c(17, 26, 17, 14, 12, 19, 13, 26, 26, 9, 14, 19, 13, 7, 18, 18, 19, 19, 20, 7, 19, 16, 15, 20, 16, 7, 19, 13, 8, 8, 14, 15, 9, 7, 8, 11, 7, 8, 22, 14, 13, 17, 19, 11, 16, 16, 20, 15, 7, 12.13, 15, 17, 1, 27, 20, 12, 15.38, 11, 15, 7, 24)
 #' )
-#' result_with_pre <- calculate_winP_pre_post(data, group_var = "group", pre_var = "pre", post_var = "post")
+#' result_with_pre <- calculate_winP(data, group_var = "group", pre_var = "pre", post_var = "post")
 #' print(result_with_pre)
-#' result_without_pre <- calculate_winP_pre_post(data, group_var = "group", post_var = "post")
+#' result_without_pre <- calculate_winP(data, group_var = "group", post_var = "post")
 #' print(result_without_pre)
 #'
 #' @export
-calculate_winP_pre_post <- function(data, group_var, post_var, pre_var = NULL) {
+
+calculate_winP <- function(data, group_var, post_var, pre_var = NULL) {
   # List of required packages
   required_packages <- c("dplyr", "sandwich", "lmtest", "broom")
-
   # Install missing packages
   for (pkg in required_packages) {
     if (!require(pkg, character.only = TRUE)) {
@@ -37,26 +37,24 @@ calculate_winP_pre_post <- function(data, group_var, post_var, pre_var = NULL) {
     }
   }
 
-  # Convert data to win fractions
-
   # Calculate overall rank
   data <- data %>%
-    arrange(!!sym(group_var), desc(!!sym(post_var)), if (!is.null(pre_var)) desc(!!sym(pre_var))) %>%
-    mutate(rpost = rank(-!!sym(post_var), ties.method = "average"))
+    arrange(.data[[group_var]], desc(.data[[post_var]])) %>%
+    mutate(rpost = rank(-.data[[post_var]], ties.method = "average"))
 
   if (!is.null(pre_var)) {
     data <- data %>%
-      mutate(rpre = rank(-!!sym(pre_var), ties.method = "average"))
+      mutate(rpre = rank(-.data[[pre_var]], ties.method = "average"))
   }
 
   # Calculate group-specific rank
   data <- data %>%
-    group_by(!!sym(group_var)) %>%
-    mutate(grpost = rank(-!!sym(post_var), ties.method = "average"))
+    group_by(.data[[group_var]]) %>%
+    mutate(grpost = rank(-.data[[post_var]], ties.method = "average"))
 
   if (!is.null(pre_var)) {
     data <- data %>%
-      mutate(grpre = rank(-!!sym(pre_var), ties.method = "average"))
+      mutate(grpre = rank(-.data[[pre_var]], ties.method = "average"))
   }
 
   data <- data %>%
@@ -64,10 +62,10 @@ calculate_winP_pre_post <- function(data, group_var, post_var, pre_var = NULL) {
 
   # Calculate win fractions
   freqcnt <- data %>%
-    group_by(!!sym(group_var)) %>%
+    group_by(.data[[group_var]]) %>%
     summarise(count = n()) %>%
-    mutate(!!sym(group_var) := 1 - !!sym(group_var)) %>%
-    arrange(!!sym(group_var))
+    mutate(across(all_of(group_var), ~ 1 - .)) %>%
+    arrange(.data[[group_var]])
 
   data <- data %>%
     left_join(freqcnt, by = group_var) %>%
@@ -79,11 +77,13 @@ calculate_winP_pre_post <- function(data, group_var, post_var, pre_var = NULL) {
   }
 
   # Perform regression on win fractions
-  if (!is.null(pre_var)) {
-    model2 <- lm(winf ~ winf_pre + get(group_var), data = data)
+  formula <- if (!is.null(pre_var)) {
+    as.formula(paste("winf ~ winf_pre +", group_var))
   } else {
-    model2 <- lm(winf ~ get(group_var), data = data)
+    as.formula(paste("winf ~", group_var))
   }
+
+  model2 <- lm(formula, data = data)
 
   # Extract coefficients and standard errors using HC2 standard errors
   hc_se <- coeftest(model2, vcov = vcovHC(model2, type = "HC2"))
